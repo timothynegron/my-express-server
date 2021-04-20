4cbd84404e3402c73702f1b8aa7168050f5aebed
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-20 15:57:35 -0400

    Adds code to handle request to home root

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+// Incorporate express module to file
 const express = require("express");
 
+// Bind function that represents express module
 const app = express();
 
-// Listen to any request on http
-app.listen(3000)
+// Create port number
+const port = 3002;
+
+// What should happen when someone makes a get request to the home root
+app.get("/", function(request, response){
+    // What to do when the request to the home root occurs
+    response.send("<h1>Hello, World!</h1>");
+})
+
+// Listen on a specific port for any http request that gets sent to this server
+app.listen(port, function(){
+    console.log("Server started on port " + port);
+});
+
